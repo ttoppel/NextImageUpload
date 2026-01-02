9a654a0bfcf7c6610e78ba74148617681c5ad188
ttoppel <tom@toppel.com>
2026-01-02 13:45:57 -0700

    next config source maps

---
+++ next.config.js
@@ -0,0 +1,23 @@
+
+
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  experimental: {
+    serverSourceMaps: true,
+    serverActions: {
+      bodySizeLimit: '10mb',
+    },
+  },
+   typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+}
+ 
+module.exports = nextConfig
